3a7f4c5453ab94e200692bc302e66699760b8015
PolyMeilex <marynczakbartlomiej@gmail.com>
2024-03-03 03:37:47 +0100

    Improve delegate example

--- wayland-client/examples/delegated.rs
+++ wayland-client/examples/delegated.rs
@@ -9,6 +9,12 @@ use wayland_client::{
     Connection, Dispatch, Proxy, QueueHandle,
 };
 
+/// A demonstration of how delegateing can make implementing protocols an implementation detail
+///
+/// Users of this module only need to implement `RegistryHandler` trait on their state,
+/// Implementation of `Dispatch` can remain an internal detail of the module.
+///
+/// In a way you can pretend that everything inside of this submodule is a library / different crate
 mod delegated {
     use super::*;
 
@@ -22,9 +28,13 @@ mod delegated {
     }
 
     impl Registry {
+        /// Create a [`WlRegistry`] object, and handle it's events internally
+        /// It can use [`RegistryHandler`] trait to callback to your `D` state.
         pub fn new<D: RegistryHandler>(qh: &QueueHandle<D>, display: &WlDisplay) -> Self {
+            // Let's construct a `WlRegistry` object that dispatches it's events to our
+            // `Registry::event` rather than to `D`,
+            // that way it can remain an implementation detail
             let data = qh.make_data::<WlRegistry, _, Self>(());
-
             let wl_registry =
                 display.send_constructor(wl_display::Request::GetRegistry {}, data).unwrap();
 
@@ -37,6 +47,8 @@ mod delegated {
     }
 
     impl<D: RegistryHandler> Dispatch<WlRegistry, (), D> for Registry {
+        /// Called whenever an object created via `make_data<WlRegistry, _, Registry>`
+        /// receives a server event
         fn event(
             state: &mut D,
             _: &wl_registry::WlRegistry,
@@ -46,6 +58,7 @@ mod delegated {
             _: &QueueHandle<D>,
         ) {
             if let wl_registry::Event::Global { name, interface, version } = event {
+                // Let's callback the user of this abstraction, informing them about new global
                 state.new_global(name, &interface, version);
             }
         }
@@ -62,6 +75,8 @@ impl delegated::RegistryHandler for AppData {
         &mut self.registry
     }
 
+    // Even tho we did not implement WlRegistry, `delegated::Registry` implemented it for us,
+    // and will call this method whenever new globals appear
     fn new_global(&mut self, name: u32, interface: &str, version: u32) {
         println!("[{}] {} (v{})", name, interface, version);
 
@@ -94,6 +109,8 @@ fn main() {
     let mut event_queue = conn.new_event_queue::<AppData>();
     let qh = event_queue.handle();
 
+    // Let's ask `delegated::Registry` to implement `WlRegistry` for us, only calling us back whenever
+    // necessary via `RegistryHandler` trait
     let registry = delegated::Registry::new(&qh, &display);
 
     let mut app = AppData { registry, qh: qh.clone() };
--- wayland-server/examples/delegated.rs
+++ wayland-server/examples/delegated.rs
@@ -1,6 +1,14 @@
+#![allow(clippy::single_match)]
+
 use delegated::{OutputHandler, OutputManagerState};
 use wayland_server::Display;
 
+/// A demonstration of how delegateing can make implementing protocols an implementation detail
+///
+/// Users of this module only need to implement `OutputHandler` trait on their state,
+/// Implementations of `GlobalDispatch` and `Dispatch` can remain an internal detail of the module.
+///
+/// In a way you can pretend that everything inside of this submodule is a library / different crate
 mod delegated {
     use wayland_server::{
         backend::GlobalId,
@@ -10,7 +18,7 @@ mod delegated {
 
     pub trait OutputHandler {
         fn state(&mut self) -> &mut OutputManagerState;
-        fn some_callback(&mut self);
+        fn some_callback_one_might_need(&mut self);
     }
 
     pub struct OutputManagerState {
@@ -18,20 +26,28 @@ mod delegated {
     }
 
     impl OutputManagerState {
+        /// Create a [`WlOutput`] global, and handle it's requests internally
+        /// It can use [`OutputHandler`] trait to callback to your `D` state.
         pub fn create_delegated_global<D>(dh: &DisplayHandle) -> Self
         where
             D: OutputHandler + 'static,
         {
+            // Let's create a global that dispatches it's bind requests to our
+            // `OutputManagerState::bind` rather than to `D`,
+            // that way it can remain an implementation detail
             let global_id = dh.create_delegated_global::<D, WlOutput, (), Self>(4, ());
             Self { global_id }
         }
 
-        pub fn gloabl_id(&self) -> GlobalId {
+        /// Let's expose global id to the user, so they can remove the global when needed
+        pub fn global_id(&self) -> GlobalId {
             self.global_id.clone()
         }
     }
 
     impl<D: OutputHandler> GlobalDispatch<WlOutput, (), D> for OutputManagerState {
+        /// Called whenever a global created via `create_delegated_global<_, WlOutput, _, OutputManagerState>`
+        /// gets binded by a client
         fn bind(
             state: &mut D,
             _handle: &DisplayHandle,
@@ -40,23 +56,35 @@ mod delegated {
             _global_data: &(),
             data_init: &mut DataInit<'_, D>,
         ) {
+            // Let's init the object as delegated as well, that way it will dispatch it's requests to our `OutputManagerState::request`
+            // rather than to `D`
             let _output = data_init.init_delegated::<_, _, Self>(resource, ());
 
-            state.state();
-            state.some_callback();
+            // Wa can easily callback the `D` from here if needed, eg.
+            let _some_state_one_might_need = state.state();
+            state.some_callback_one_might_need();
         }
     }
 
-    impl<D> Dispatch<WlOutput, (), D> for OutputManagerState {
+    impl<D: OutputHandler> Dispatch<WlOutput, (), D> for OutputManagerState {
+        /// Called whenever an object created via `init_delegated<WlOutput, _, OutputManagerState>`
+        /// receives a client request
         fn request(
-            _state: &mut D,
+            state: &mut D,
             _client: &Client,
             _resource: &WlOutput,
-            _request: wl_output::Request,
+            request: wl_output::Request,
             _data: &(),
             _dhandle: &DisplayHandle,
             _data_init: &mut DataInit<'_, D>,
         ) {
+            match request {
+                wl_output::Request::Release => {
+                    // Wa can easily callback the `D` from here if needed, eg.
+                    state.some_callback_one_might_need();
+                }
+                _ => {}
+            }
         }
     }
 }
@@ -70,15 +98,17 @@ impl OutputHandler for App {
         &mut self.output_state
     }
 
-    fn some_callback(&mut self) {}
+    fn some_callback_one_might_need(&mut self) {}
 }
 
 fn main() {
     let display = Display::<App>::new().unwrap();
 
+    // Let's ask `OutputManagerState` to implement `WlOutput` for us, only calling us back whenever
+    // necessary via `OutputHandler` trait
     let output_state = OutputManagerState::create_delegated_global::<App>(&display.handle());
 
     let app = App { output_state };
 
-    display.handle().remove_global::<App>(app.output_state.gloabl_id());
+    display.handle().remove_global::<App>(app.output_state.global_id());
 }
